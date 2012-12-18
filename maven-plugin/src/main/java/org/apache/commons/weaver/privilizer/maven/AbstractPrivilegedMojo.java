12dfddcf18370647077fbcd3bc89d2e9574779e9
Mark Struberg <struberg@apache.org>
2012-12-18 20:24:23 +0000

    fix module structure
    of course this will now break the build temporarily :/
    
    
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/privilizer/trunk@1423618 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-plugin/src/main/java/org/apache/commons/weaver/privilizer/maven/AbstractPrivilegedMojo.java
@@ -0,0 +1,89 @@
+/*
+ *  Copyright the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.commons.weaver.privilizer.maven;
+
+import java.io.File;
+import java.net.URLClassLoader;
+import java.util.List;
+
+import org.apache.commons.weaver.privilizer.weaver.AccessLevel;
+import org.apache.commons.weaver.privilizer.weaver.FilesystemPrivilizer;
+import org.apache.commons.weaver.privilizer.weaver.URLArray;
+import org.apache.commons.weaver.privilizer.weaver.Privilizer.Log;
+import org.apache.commons.weaver.privilizer.weaver.Privilizer.Policy;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+
+/**
+ * Defines common properties.
+ */
+public abstract class AbstractPrivilegedMojo extends AbstractMojo {
+    /**
+     * Weaving policy to use.
+     */
+    @Parameter(property = "privilizer.policy", required = true, defaultValue = "DYNAMIC")
+    protected Policy policy;
+
+    @Parameter(defaultValue = "false")
+    protected boolean verbose;
+
+    protected abstract List<String> getClasspath();
+
+    protected abstract File getTarget();
+
+    protected abstract AccessLevel getAccessLevel();
+
+    protected FilesystemPrivilizer createWeaver() {
+        return new FilesystemPrivilizer(policy, new URLClassLoader(URLArray.fromPaths(getClasspath())), getTarget()) {
+            @Override
+            protected boolean permitMethodWeaving(AccessLevel accessLevel) {
+                return getAccessLevel().compareTo(accessLevel) <= 0;
+            }
+        }.loggingTo(new Log() {
+
+            @Override
+            public void info(String message) {
+                getLog().info(message);
+            }
+
+            @Override
+            public void error(String message) {
+                getLog().error(message);
+            }
+
+            @Override
+            public void debug(String message) {
+                getLog().debug(message);
+            }
+
+            @Override
+            public void verbose(String message) {
+                if (verbose) {
+                    getLog().info(message);
+                } else {
+                    getLog().debug(message);
+                }
+            }
+
+            @Override
+            public void warn(String message) {
+                getLog().warn(message);
+            }
+        });
+    }
+
+}
