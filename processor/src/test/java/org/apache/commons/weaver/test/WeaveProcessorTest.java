5bb1f8732298bf4e2c8ec6d49de170e5265aaa93
Mark Struberg <struberg@apache.org>
2012-12-19 21:50:45 +0000

    add a unit test for a test Weaver
    git-svn-id: https://svn.apache.org/repos/asf/commons/sandbox/privilizer/trunk@1424159 13f79535-47bb-0310-9956-ffa450edef68

---
+++ processor/src/test/java/org/apache/commons/weaver/test/WeaveProcessorTest.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.commons.weaver.test;
+
+import java.util.Properties;
+
+import org.apache.commons.weaver.test.beans.TestBeanWithClassAnnotation;
+import org.apache.commons.weaver.test.beans.TestBeanWithMethodAnnotation;
+import org.apache.commons.weaver.test.weaver.TestWeaver;
+import org.apache.commons.weaver.WeaveProcessor;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Test the {@link WeaveProcessor}
+ */
+public class WeaveProcessorTest extends WeaverTestBase
+{
+
+    @Test
+    public void testWeaveVisiting() throws Exception {
+        addClassForScanning(TestBeanWithMethodAnnotation.class);
+        addClassForScanning(TestBeanWithClassAnnotation.class);
+
+        WeaveProcessor wp = WeaveProcessor.getInstance();
+
+        Properties config = new Properties();
+        config.put("configKey", "configValue");
+        wp.configure(config);
+
+        wp.addClassPath(getTargetFolder());
+
+        TestWeaver.postWeaveExecuted = false;
+        TestWeaver.preWeaveExecuted = false;
+        TestWeaver.wovenClasses.clear();
+        TestWeaver.wovenMethods.clear();
+
+        wp.weave();
+
+        Assert.assertTrue(TestWeaver.preWeaveExecuted);
+        Assert.assertTrue(TestWeaver.postWeaveExecuted);
+        Assert.assertEquals(1, TestWeaver.wovenClasses.size());
+        Assert.assertEquals(TestBeanWithClassAnnotation.class, TestWeaver.wovenClasses.get(0));
+
+        Assert.assertEquals(1, TestWeaver.wovenMethods.size());
+        Assert.assertEquals(TestBeanWithMethodAnnotation.class, TestWeaver.wovenMethods.get(0));
+    }
+}
---
+++ processor/src/test/java/org/apache/commons/weaver/test/weaver/TestWeaver.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.commons.weaver.test.weaver;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.commons.weaver.test.beans.TestAnnotation;
+import org.junit.Assert;
+
+import org.apache.commons.weaver.spi.Weaver;
+
+/**
+ */
+public class TestWeaver implements Weaver
+{
+    public static boolean preWeaveExecuted = false;
+    public static boolean postWeaveExecuted = false;
+    public static List<Method> wovenMethods = new ArrayList<Method>();
+    public static List<Class> wovenClasses = new ArrayList<Class>();
+
+    @Override
+    public void configure(Properties config)
+    {
+        Assert.assertNotNull(config);
+        Assert.assertEquals(1, config.size());
+
+        String configValue = config.getProperty("configKey");
+        Assert.assertEquals("configValue", configValue);
+    }
+
+    @Override
+    public List<Class<? extends Annotation>> getInterest()
+    {
+        List<Class<? extends Annotation>> interests = new ArrayList<Class<? extends Annotation>>();
+        interests.add(TestAnnotation.class);
+        return interests;
+    }
+
+    @Override
+    public void preWeave()
+    {
+        preWeaveExecuted = true;
+    }
+
+    @Override
+    public boolean weave(Class classToWeave)
+    {
+        return wovenClasses.add(classToWeave);
+    }
+
+    @Override
+    public boolean weave(Method methodToWeave)
+    {
+        return wovenMethods.add(methodToWeave);
+    }
+
+    @Override
+    public void postWeave()
+    {
+        postWeaveExecuted = true;
+    }
+}
