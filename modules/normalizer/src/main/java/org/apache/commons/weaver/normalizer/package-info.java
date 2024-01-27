e42bec2efd59791fb5fdbc329e109348e315f8fd
Gary Gregory <garydgregory@gmail.com>
2024-01-27 09:03:01 -0500

    Whitespace

--- modules/normalizer/src/main/java/org/apache/commons/weaver/normalizer/package-info.java
+++ modules/normalizer/src/main/java/org/apache/commons/weaver/normalizer/package-info.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * Implements the Normalizer weaver, whose purpose is to collapse identical
  * anonymous class definitions into a single one.
